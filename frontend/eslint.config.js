0508896343bd6440f353e3af1ae214007b37efe5
Ray Hamilton <ramone.d.hamilton@gmail.com>
2025-11-21 17:40:47 -0500

    Update ESLint config to exclude test files from linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', '.vite', 'wailsjs']),
+  globalIgnores(['dist', '.vite', 'wailsjs', '**/*.test.{ts,tsx}', '**/test/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
